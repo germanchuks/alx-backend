d57311ed9281df60da53c564c8372f33a337758c
Daniel German <danielchuksgerman@gmail.com>
2024-05-28 09:20:01 +0000

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,14 +2,25 @@
 """Basic Flask app"""
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
 
 
 @app.route('/')
 def index():
     """Outputs `Hello world` as header"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
